b7c52e36c6ea1190769593cdf509f1b29a29043d
pudin <braian.luques2@gmail.com>
2025-07-20 06:17:21 -0300

    SQL connected

--- database/config.js
+++ database/config.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2')
 require('dotenv').config()
 
 const config = {
-    host: '',
-    user: '',
-    password: '',
-    database: ''
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 }
 const db = mysql.createConnection(config)
 
